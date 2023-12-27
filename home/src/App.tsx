9c0f70d8dd8e4584142cb0361049714ad80bf354
Samuel Littrell <samuel.littrell@berkeley.edu>
2023-12-27 15:37:13 -0800

    website build

--- home/src/App.tsx
+++ home/src/App.tsx
@@ -25,6 +25,15 @@ function App() {
   const experiences: Experience[] = [
     // Add your experiences here
     // Example: { role: 'Software Developer', company: 'XYZ Inc.', description: 'Worked on...', period: 'Jan 2020 - Present' },
+    { role: 'Software R&D Intern', company: 'Belden Inc.', 
+    description: `- Developed comprehensive automated user-interface testing suite for Belden Horizon Mobile App utilizing Java, Kotlin, \n
+                    JavaScript, and Android Studio, enhancing software quality and reducing manual testing time by over 90%\n\n
+                  - Engineered robust test automation framework that executes functional, integration, and end-to-end UI tests\n\n
+                  - Created and executed over 200 automated UI test scripts in Java and JavaScript using Espresso, Appium, and Selenium\n\n
+                  - Identified and fixed critical bugs during testing sprints, resulting in a 20% decrease in user-reported issues\n\n
+                  - Documented detailed test plans and cases following best practices for maintainability, driving collaboration across team\n\n
+                  `, 
+    period: 'May 2023 - Aug 2023' }
   ];
 
   return (
